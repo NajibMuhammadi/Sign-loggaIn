973abe06f548d015103eda866409a262f053ae05
Najib Muhammadi <najibmuhammadi003@gmail.com>
2024-05-22 21:13:17 +0200

    La till en kontroll för inputs med hjälp av Joi

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import connectDataBase from './db.js';
+import Joi from 'joi';
 
 const app = express();
 const PORT = process.env.PORT || 8083;
@@ -9,7 +10,12 @@ const PORT = process.env.PORT || 8083;
 app.use(cors());
 app.use(express.json());
 
-
+const schema = Joi.object({
+    FullName: Joi.string().min(3).max(30).required(),
+    EmailId: Joi.string().email().required(),
+    Password: Joi.string().min(6).required(),
+    ConfirmPassword: Joi.string().required()
+});
 app.get('/', (req, res) => {
     const sql = "SELECT * FROM signup;";
 
@@ -25,6 +31,10 @@ app.get('/', (req, res) => {
 
 app.post('/create', async (req, res) => {
     const { FullName, EmailId, Password, ConfirmPassword } = req.body;
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).send({ error: error.details[0].message });
+    }
     if (Password !== ConfirmPassword) {
         return res.status(400).send({ error: 'Passwords do not match' });
     }
